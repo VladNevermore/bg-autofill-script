b0f49bb0993b2479fba834338fc8dbee453c3dcf
VladNevermore <151689422+VladNevermore@users.noreply.github.com>
2025-08-10 18:54:45 +0500

    Update main.js
    68863

--- scr/main.js
+++ scr/main.js
@@ -1,1 +1,15 @@
+import { initCrmFinleo } from "./sites/crm.js";
+import { initIngoBank } from "./sites/ingobank.js";
 
+(function() {
+  "use strict";
+
+  const url = window.location.href;
+
+  if (url.includes("crm.finleo.ru/orders/")) {
+    initCrmFinleo();
+  }
+  if (url.includes("market.bg.ingobank.ru/tasks")) {
+    initIngoBank();
+  }
+})();
